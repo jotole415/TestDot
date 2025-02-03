502cb3b840ec75ac3cb0be631f27efae0a846f88
jotole415 <58862745+jotole415@users.noreply.github.com>
2025-02-03 21:32:13 +0000

    atualização

--- cypress/e2e/adicionar_produto.cy.js
+++ cypress/e2e/adicionar_produto.cy.js
@@ -1,6 +1,6 @@
-describe('Adicionar produto ao carrinho na Amazon', () => {
-  it('Deve adicionar um produto ao carrinho', () => {
-     cy.visit('https://www.amazon.com.br');
+describe('Busca de produtos na Amazon', () => {
+  it('Deve buscar um produto e adicionar no carrinho', () => {
+    cy.visit('https://www.amazon.com.br');
     cy.buscarProduto('BIC Caneta Esferográfica Escrita Fina Cristal Fina Azul');
     cy.get('[data-image-index="7"]').click(); // Seleciona o produto usando um seletor CSS
     cy.adicionarAoCarrinho();
